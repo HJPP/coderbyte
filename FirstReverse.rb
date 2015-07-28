2f57a5f0600180e8334e88c56739c8e49bb28565
HJPP <smart0822@gmail.com>
2015-07-28 13:38:34 +0900

    Create FirstReverse.rb

---
+++ FirstReverse.rb
@@ -0,0 +1,12 @@
+function FirstReverse(str) { 
+
+  // code goes here  
+  return str.reverse
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());                            
+
+
